a1ddf177c9607b287b1c2e80c0f37096668dd8fd
weizhengwei <iamchingwei@gmail.com>
2017-06-09 18:06:10 +0800

    init project

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, send_from_directory
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+
+@app.route('/favicon.ico')
+def favicon():
+	return send_from_directory(os.path.join(app.root_path, 'static'),
+			'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+if __name__ == '__main__':
+	app.run(debug=True, port=12345, host='0.0.0.0')
